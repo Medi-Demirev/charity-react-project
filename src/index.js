0f352b4179b053b386af633e989eba5c56347bd5
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-04 12:44:34 +0300

    adding basic  css for the application

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './css/font-awesome.min.css';
+import './css/themify-icons.css';
+import './css/flaticon.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
