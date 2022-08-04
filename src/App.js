2851763eafabc6f07227771c59e9adfd91c0b5e8
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-04 15:59:22 +0300

    Add site logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import HeaderTopbar from './components/HeaderTopbar/HeaderTopbar';
+import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <HeaderTopbar/>
+      <Header/>
       <Footer/>
 
     </div>
