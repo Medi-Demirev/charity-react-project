97c325f25634378e2d70e1bda425cc61d3fc2944
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-04 14:41:34 +0300

    Add Footer components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './Footer.css'
+
+const Footer = (props) =>{
+
+  return(
+    <footer className="tp-site-footer">  
+        <div className="tp-lower-footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col col-xs-12">
+                        <p className="copyright">&copy; 2022 Kindness. All rights reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+  )
+} 
+
+export default Footer;
