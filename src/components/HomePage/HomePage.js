4a86e369938b07cecb705ea501b2ad0c97e7cd93
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-05 23:28:52 +0300

    Add event  section to home page.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import Causes from "../Causes/Causes";
+import EventPage from "../Event/EventPage";
 
 const HomePage =() => {
     return(
         <>
          
             <Causes/>
+            <EventPage/>
           
         </>
     )
