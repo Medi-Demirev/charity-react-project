05eb4e53067a169bdda9d930c67ddc74d8d53b8c
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-08 06:23:13 +0300

    Logic for login operation

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,14 +1,43 @@
 
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useContext } from 'react';
+
+import { Link, useNavigate} from 'react-router-dom';
+import * as authService from "../../services/authService";
+import { AuthContext } from '../../contexts/AuthContext';
+
 import './LoginPage.css'
 import Logo from '../../assets/Logo.png'
 
 const LoginPage = () =>{
+const { userLogin } = useContext(AuthContext);
+const navigate = useNavigate();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const {
+        email,
+        password,
+    } = Object.fromEntries(new FormData(e.target));
+          authService.login(email, password)
+          .then(authData =>{
+            userLogin(authData);
+            navigate('/')
+          })
+        
+        .catch(() => {
+          
+            navigate('/404');
+        })
+
+   
+  };
+  
     return (
       
       <section id="loginPage">
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={onSubmit}>
       <img src={Logo} alt="Logo"/>
         <h2>Login</h2>
         <div>
@@ -34,7 +63,7 @@ const LoginPage = () =>{
         <button className="btn" type="submit">
           Login
         </button>
-        <p className="register-link">
+        <p className="field">
           <span>
             If you don't have profile click <Link to="/register">here</Link>
           </span>
