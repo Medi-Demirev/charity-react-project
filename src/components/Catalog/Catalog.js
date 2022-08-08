fae179a1d8ef82e93373fc349896c5248b0c4209
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-08 18:17:52 +0300

    Add Catalog component

---
+++ src/components/Catalog/Catalog.js
@@ -0,0 +1,37 @@
+import { useContext } from 'react';
+import { CauseContext } from '../../contexts/CauseContext';
+import CauseItem from '../CauseItem/CauseItem';
+
+import './Catalog.css'
+
+const Catalog = () => {
+ const {causes} = useContext(CauseContext)
+ 
+    return(
+          
+      <div className="case-area section-padding">
+      <div className="container">
+        <div className="col-lg-6 offset-lg-3">
+          <div className="section-title section-title2 text-center">
+            <div className="thumb-text">
+              <span>CAUSES</span>
+            </div>
+            <h2>RECENT CAUSES</h2>
+            <p>
+            Every heart is charitable by nature. Let's do more good deeds for needy children. 
+            To be their hope for a better and happy life.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+       {causes.length > 0
+       ? causes.map(x => <CauseItem key={x._id} cause ={x}/>)
+      :<h3 className='no-causes'>NO ACTIVE CASES YET!</h3>}
+      </div> 
+    </div>
+    </div>
+      
+    );
+};
+
+export default Catalog;
