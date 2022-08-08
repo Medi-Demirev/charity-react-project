d0b6ff8943d340555c3e74f90875deb8d181721f
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-09 00:13:11 +0300

    Add team page component

--- src/components/Team/TeamPage.js
+++ src/components/Team/TeamPage.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import { useContext,} from 'react';
+import { VolunteerContext } from '../../contexts/VolunteerContext';
+import VolunteerProfile from './VolunteerProfile/VolunteerProfile';
 
 import './TeamPage.css';
 
-import tm1 from '../../assets/11.jpg';
-import tm2 from '../../assets/221.jpg';
-import tm3 from '../../assets/33.jpg';
-import tm4 from '../../assets/44.jpg';
+const TeamPage = () => {
+    const {volunteer} = useContext(VolunteerContext);
 
-const TeamPage = (props) => {
-
-    const ClickHandler = () =>{
-        window.scrollTo(10, 0);
-     }
-
-    return(
-        <div  className={`volunteer-area section-padding ${props.addclass}`}>
+    return (
+        <div  className={`volunteer-area section-padding `}>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6 offset-lg-3">
@@ -30,55 +23,15 @@ const TeamPage = (props) => {
                 </div>
                 <div className="volunteer-wrap">
                     <div className="row">
-                        <div className="col col-lg-3 col-md-6 custom-grid col-12">
-                            <div className="volunteer-item">
-                                <div className="volunteer-img">
-                                    <img src={tm1} alt="" />
-                                </div>
-                                <div className="volunteer-content">
-                                    <h2><Link onClick={ClickHandler} to="/volunteer">Adriane Newby</Link></h2>
-                                    <span>Volunteer</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-lg-3 col-md-6 custom-grid col-12">
-                            <div className="volunteer-item">
-                                <div className="volunteer-img">
-                                    <img src={tm2} alt="" />
-                                </div>
-                                <div className="volunteer-content">
-                                    <h2><Link onClick={ClickHandler} to="/volunteer">Allene Castaneda</Link></h2>
-                                    <span>Volunteer</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-lg-3 col-md-6 custom-grid col-12">
-                            <div className="volunteer-item">
-                                <div className="volunteer-img">
-                                    <img src={tm3} alt="" />
-                                </div>
-                                <div className="volunteer-content">
-                                    <h2><Link onClick={ClickHandler} to="/volunteer">Malinda Willoughby</Link></h2>
-                                    <span>Volunteer</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-lg-3 col-md-6 custom-grid col-12">
-                            <div className="volunteer-item">
-                                <div className="volunteer-img">
-                                    <img src={tm4} alt="" />
-                                </div>
-                                <div className="volunteer-content">
-                                    <h2><Link onClick={ClickHandler} to="/volunteer">Wilburn Hatfield</Link></h2>
-                                    <span>Volunteer</span>
-                                </div>
-                            </div>
-                        </div>
+                    {volunteer.length > 0
+                      ? volunteer.slice(0,4).map(x => <VolunteerProfile key={x._id} volunteer ={x}/>)
+                     :<h3 className='no-vounteer'>NO ACTIVE CASES YET!</h3>}
+                        
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default TeamPage;
