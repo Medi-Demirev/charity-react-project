cc7693fc0c73e88d298cac4b34c62b27b4cbaa9b
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-09 00:12:44 +0300

    Add volunteer profile component

---
+++ src/components/Team/VolunteerProfile/VolunteerProfile.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+const VolunteerProfile = ({volunteer}) => {
+    return (
+        <div className="col col-lg-3 col-md-6 custom-grid col-12">
+                            <div className="volunteer-item">
+                                <div className="volunteer-img">
+                                    <img src={volunteer.imageProfile} alt="" />
+                                </div>
+                                <div className="volunteer-content">
+                                    <h2><Link  to="/volunteer">{volunteer.fullName}</Link></h2>
+                                    <span>Volunteer</span>
+                                </div>
+                            </div>
+                        </div>
+    );
+};
+export default VolunteerProfile;
