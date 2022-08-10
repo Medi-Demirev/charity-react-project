bc6dfb6688437e95e34eeb0537c3c226eec35255
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-10 08:29:16 +0300

    Move auth logic from app to  auth context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,35 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children,
+}) => {
+    const [auth, setAuth] = useLocalStorage('auth', {});
+
+    const userLogin = (authData) => {
+        setAuth(authData);
+        
+    };
+
+    const userLogout = () => {
+        setAuth({});
+    };
+    const userRegister = (authData) => {
+        setAuth(authData);
+        
+    };
+
+    return (
+        <AuthContext.Provider value={{
+            user: auth,
+            userLogin,
+            userRegister,
+            userLogout,
+            isAuthenticated: !!auth.accessToken
+        }}>
+            {children}
+        </AuthContext.Provider>  
+    );
+};
