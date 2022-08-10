301464f0f36b1e3a2c21a6d7bedd219d1068abe1
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-10 08:36:46 +0300

    Add auth provider

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { useLocalStorage } from './hooks/useLocalStorage';
 import { CauseContextProvider } from './contexts/CauseContext';
 import { VolunteerContextProvider } from './contexts/VolunteerContext';
 import { EventContextProvider } from './contexts/EventContext';
+import { AuthProvider } from './contexts/AuthContext';
+
 
 import ContactPage from './components/ContactPage/ContactPage';
 import Login from './components/LoginPage/LoginPage';
@@ -16,41 +18,40 @@ import HeaderTopbar from './components/HeaderTopbar/HeaderTopbar';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import EventCatalog from './components/Event/EventsCatalog/EventsCatalog';
-
+import EventDetails from './components/Event/EventDetails/EventDetails';
 import Logout from './components/Logout/Logout';
 import CausesCatalog from './components/Causes/CausesCatalog/CausesCatalog';
+import CauseDetails from './components/Causes/CauseDetails/CauseDetails';
 import VolunteersCatalog from './components/Team/VolunteersCatalog/VolunteersCatalog';
+
 
 import './App.css';
 
 function App() {
-  const [auth, setAuth] = useLocalStorage('auth', {});
 
-  const userLogin = (authData) => {
-      setAuth(authData);
-  };
-
-  const userLogout = () => {
-    setAuth({});
-};
   return (
-    <AuthContext.Provider value={{user:auth, userLogin, userLogout}}>
+    <AuthProvider>
     <div className="App">
+      
     <CauseContextProvider>
     <VolunteerContextProvider>
     <EventContextProvider>
       <HeaderTopbar/>
       <Header/>
           <Routes>
-             <Route path='/login' element={<Login/>}/>;
-             <Route path='/register' element={<RegisterPage/>}/>;
+             <Route path='/login' element={<Login/>}/>
+             <Route path='/register' element={<RegisterPage/>}/>
              <Route path='/logout' element={<Logout/>}/>
-             <Route path='/contact' element={<ContactPage/>}/>;
-             <Route path='/about' element={<About/>}/>;
-             <Route path='/' element={<HomePage/>}/>;
-             <Route path='/all-causes' element={<CausesCatalog/>}/>;
-             <Route path='/all-events' element={<EventCatalog/>}/>;
-             <Route path='/all-volunteers' element={<VolunteersCatalog/>}/>;
+             <Route path='/contact' element={<ContactPage/>}/>
+             <Route path='/about' element={<About/>}/>
+             <Route path='/' element={<HomePage/>}/>
+             <Route path='/all-causes' element={<CausesCatalog/>}/>
+             <Route path='/all-events' element={<EventCatalog/>}/>
+             <Route path='/all-volunteers' element={<VolunteersCatalog/>}/>
+             <Route path='/event/:id' element={<EventDetails />}/>
+             <Route path='/all-events/event/:id' element={<EventDetails />}/>
+             <Route path='/cause/:id' element={<CauseDetails />}/>
+             <Route path='/all-causes/cause/:id' element={<CauseDetails />}/>
              <Route path='/*' element={<ErrorPage/>}/>;
          
         
@@ -60,8 +61,9 @@ function App() {
     </EventContextProvider>
     </VolunteerContextProvider>
     </CauseContextProvider>
+   
     </div>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
