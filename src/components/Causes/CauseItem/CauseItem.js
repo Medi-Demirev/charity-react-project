eba8b261ff1f53e57c881357aaaf5d24677012cb
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-10 08:33:50 +0300

    Change <a href with Link to

--- src/components/Causes/CauseItem/CauseItem.js
+++ src/components/Causes/CauseItem/CauseItem.js
@@ -1,30 +1,32 @@
+import {Link} from 'react-router-dom'
 const CauseItem = ({ cause }) => {
+    console.log(cause._id);
 
     return (
-        <div className="col-lg-4 col-md-6 col-12">
+        <div className="col-lg-4 col-md-6 col-12" >
             <div className="cause-item">
                 <div className="cause-top">
                     <div className="cause-img">
-                        <img src={cause.imageUrl} alt="" />
+                        <img   src={cause.imageUrl} alt="" />
                         <div className="case-btn">
-                            <a className="theme-btn" href="/donate">
+                            <Link className="theme-btn" to="/donate">
                                 Donate Now
                                 <i className="fa fa-angle-double-right" aria-hidden="true" />
-                            </a>
+                            </Link>
                         </div>
                     </div>
                 </div>
                 <div className="cause-text">
                     <ul>
                         <li>
-                            <a href="/">GOAL: {cause.goal}</a>
-                        </li>
+                            <Link to="/">GOAL: {cause.goal}</Link>
+                        </li >
                         <li>
-                            <a href="/">RISED: {cause.rised}</a>
+                            <Link  to="/">RISED:  {cause.rised}</Link>
                         </li>
                     </ul>
                     <h3>
-                        <a href="/case-single">{cause.title}</a>
+                        <Link  to={`cause/${cause._id}`}>{cause.title}</Link>
                     </h3>
                     <p>
                         {cause.subtitle}
