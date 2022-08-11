9e3de7d90d8cfcae9e5acec36b5c93ecc10f5636
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-11 20:04:25 +0300

    Add get one and create functionality to cause service

--- src/services/causeService.js
+++ src/services/causeService.js
@@ -3,3 +3,7 @@ import * as request from "./util/requester";
 const baseUrl = 'http://localhost:3030/data/causes';
 
 export const getAll = () => request.get(baseUrl);
+
+export const getOne = (causeId) => request.get(`${baseUrl}/${causeId}`);
+
+export const create = (causeData) => request.post(baseUrl, causeData);
