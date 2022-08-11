585440b707d0660a03a2d5f77fc5b23d359bbd91
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-12 01:23:35 +0300

    Add create and edit functionality to cause service

--- src/services/causeService.js
+++ src/services/causeService.js
@@ -7,3 +7,5 @@ export const getAll = () => request.get(baseUrl);
 export const getOne = (causeId) => request.get(`${baseUrl}/${causeId}`);
 
 export const create = (causeData) => request.post(baseUrl, causeData);
+
+export const edit = (causeId, causeData) => request.put(`${baseUrl}/${causeId}`, causeData);
