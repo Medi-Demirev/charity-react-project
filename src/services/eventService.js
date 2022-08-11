4797137bb1f6e28b9f7e440e273fc6e29dd936b6
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-11 18:22:20 +0300

    Add more options in event service

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -3,3 +3,9 @@ import * as request from "./util/requester";
 const baseUrl = 'http://localhost:3030/data/events';
 
 export const getAll = () => request.get(baseUrl);
+
+export const getOne = (eventId) => request.get(`${baseUrl}/${eventId}`);
+
+export const create = (eventData) => request.post(baseUrl, eventData);
+
+export const edit = (eventId, eventData) => request.put(`${baseUrl}/${eventId}`, eventData);
