dda2a93b188e9b43af4de6b7c8887306cc0673d5
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-13 00:44:39 +0300

    Add edit and remove functionality to cause service

--- src/services/causeService.js
+++ src/services/causeService.js
@@ -9,3 +9,5 @@ export const getOne = (causeId) => request.get(`${baseUrl}/${causeId}`);
 export const create = (causeData) => request.post(baseUrl, causeData);
 
 export const edit = (causeId, causeData) => request.put(`${baseUrl}/${causeId}`, causeData);
+
+export const remove = (causeId) => request.del(`${baseUrl}/${causeId}`);
