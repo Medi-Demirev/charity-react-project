6ca4ea60e16ab50e59e443dbd6139cbaae7b3945
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-15 17:49:04 +0300

    Add more functionality to volunteer service

--- src/services/volunteerService.js
+++ src/services/volunteerService.js
@@ -5,3 +5,9 @@ const baseUrl = 'http://localhost:3030/data/volunteers';
 export const getAll = () => request.get(baseUrl);
 
 export const getOne = (volunteerId) => request.get(`${baseUrl}/${volunteerId}`);
+
+export const create = (volunteertData) => request.post(baseUrl, volunteertData);
+
+export const edit = (volunteerId, volunteertData) => request.put(`${baseUrl}/${volunteerId}`, volunteertData);
+
+export const remove = (volunteerId) => request.del(`${baseUrl}/${volunteerId}`);
