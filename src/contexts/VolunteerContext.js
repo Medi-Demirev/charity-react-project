0aca8fcbcaa5e27f76733909f800c2942edaee21
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-15 12:13:37 +0300

    Add more functionality to service

--- src/contexts/VolunteerContext.js
+++ src/contexts/VolunteerContext.js
@@ -1,4 +1,4 @@
-import { createContext} from "react";
+import { createContext, useContext} from "react";
 import { useState, useEffect } from "react";
 import * as volunteerService from '../services/volunteerService';
 
@@ -6,12 +6,12 @@ export const VolunteerContext = createContext();
 
 export const VolunteerContextProvider = ({ children }) => {
 
-    const [volunteer, setVolunteer] = useState([]);
+    const [volunteers, setVolunteers] = useState([]);
     
     useEffect(()=>{
         volunteerService.getAll()
         .then(result=>{
-            setVolunteer(result)
+            setVolunteers(result)
           console.log(result);
          
         
@@ -21,10 +21,21 @@ export const VolunteerContextProvider = ({ children }) => {
            });
     },[]);
 
+    const selectVolunteer = (volunteerId) => {
+        return volunteers.find(x => x._id === volunteerId) || {};
+    };
+
     return (
-        <VolunteerContext.Provider value={{ volunteer, setVolunteer }}>
+        <VolunteerContext.Provider value={{ volunteers, setVolunteers, selectVolunteer }}>
             {children}
         </VolunteerContext.Provider>
     )
 };
 
+
+// Custom Hook
+export const useVolunteerContext = () => {
+    const context = useContext(VolunteerContext);
+
+    return context;
+};
