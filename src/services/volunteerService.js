1a3fe45daeaaa9d7b5ee60a9d5aac27a30aff2c2
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-15 12:15:24 +0300

    Add more functionality to service

--- src/services/volunteerService.js
+++ src/services/volunteerService.js
@@ -3,3 +3,5 @@ import * as request from "./util/requester";
 const baseUrl = 'http://localhost:3030/data/volunteers';
 
 export const getAll = () => request.get(baseUrl);
+
+export const getOne = (volunteerId) => request.get(`${baseUrl}/${volunteerId}`);
