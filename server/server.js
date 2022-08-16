9406be2ca0fdef802402ba3f8940f7801153dfe8
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-16 18:33:29 +0300

    Add user profiles collection to server

--- server/server.js
+++ server/server.js
@@ -1366,7 +1366,19 @@
             },
           
         },
-       
+       userProfiles: {
+            "gdfffhfghfgf":{
+                "name":"Medi Loshev",
+                "country":"Bulgaria",
+                "city":"Sofia", 
+                "adress":"Syedinenie 1",
+                "phone":"+359894563217",
+                "email": "peters@abv.bg",
+                "balance": "100",
+                "imageUrl":"https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                
+            },
+       },
        
         donation: {},
 
