2511854de32402077f1b580802795fce5d9b63d9
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-20 02:38:19 +0300

    Fix simple data to server

--- server/server.js
+++ server/server.js
@@ -1358,9 +1358,7 @@
                 "benefit3": "Else he endures pains to avoid worse pains.",
                 "benefit4": "We denounce with righteous indignation and dislike men.",
                 "benefit5": "Which is the same as saying through.",
-                "raised": "5800",
                 "goal": "10000",
-                "donors": "150",
                 "imageUrl": "https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
                 "_createdOn": 1617194128618
             },
@@ -1380,7 +1378,9 @@
             },
        },
        
-        donation: {},
+        donations: {
+    
+        },
 
         volunteers:{
             "hfghf54567453456867":{
