ea73152a46214dd70a4c9693ed50d5b925b97d63
Medi-Demirev <93528758+Medi-Demirev@users.noreply.github.com>
2022-08-20 19:18:31 +0300

    Add spinner component

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,17 @@
+import '../Spinner/Spinner.css'
+
+const LoadingSpinner = () =>{
+    return (
+        <body className='fullSize'>
+        <div className="rhombus">
+        <div className="rhombus2">
+          <div className="circle21" />
+          <div className="circle22" />
+        </div>
+      </div>
+      </body>
+      
+    );
+  };
+
+  export default LoadingSpinner;
